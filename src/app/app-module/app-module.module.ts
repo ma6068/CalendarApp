abbd8f8a1a2dd8149eaa310db26812b91f2ff45e
ma6068 <ma6068@student.uni-lj.si>
2024-04-20 09:37:48 +0200

    Calendar component

---
+++ src/app/app-module/app-module.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from '../app.component';
+import { CalendarComponent } from '../calendar/calendar.component';
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    AppComponent,
+    BrowserModule,
+    CommonModule,
+  ],
+  providers: [],
+  bootstrap: [
+  ]
+})
+export class AppModule { }
